b0682486a9e42d23f03cb30e96e44f40822af1ec
Bl0tniaQus <focusedmud@wp.pl>
2024-10-13 10:50:04 +0200

    koniec cz1

--- cz1/wizualizacja.py
+++ cz1/wizualizacja.py
@@ -9,6 +9,7 @@ if not os.path.exists("./wykresy"):
 
 dane = pd.read_csv("./przetwarzanie_zamienbledne.csv")
 
+#wykresy rozkładu prawdopodobieństwa każdej z bojowych statystyk
 stats = ["HP","Attack","Defense","Sp. Atk", "Sp. Def", "Speed"]
 for stat in stats:
 	dane[stat].plot.kde()
@@ -17,7 +18,8 @@ for stat in stats:
 	plt.title("Gęstość prawdopodobieństwa statystyki \""+ stat+"\"")
 	plt.savefig("./wykresy/"+stat+".jpg")
 	plt.clf()
-	
+
+#wykres zależności ataku i specjalnego ataku od obrony
 dane_wykres = dane.sort_values(by=["Defense"])[["Attack","Defense","Sp. Atk"]].groupby(dane["Defense"]).median()
 plt.plot(dane_wykres["Defense"],dane_wykres["Sp. Atk"])
 plt.plot(dane_wykres["Defense"],dane_wykres["Attack"])
@@ -28,6 +30,7 @@ plt.title("Wykres zależności ataku od fizycznej obrony")
 plt.savefig("./wykresy/atk_od_obrony.jpg")
 plt.clf()
 
+#wykres zależności specjalnego ataku od fizycznego ataku
 dane_wykres = dane.sort_values(by=["Attack"])[["Sp. Atk","Attack"]].groupby(dane["Attack"]).median()
 plt.plot(dane_wykres["Attack"],dane_wykres["Sp. Atk"])
 plt.xlabel("Atak fizyczny")
@@ -36,13 +39,23 @@ plt.title("Wykres zależności ataku fizycznego od ataku specjalnego")
 plt.savefig("./wykresy/atk_od_spatk.jpg")
 plt.clf()
 
+colors = {"Fire" : "orangered", "Water" : "aqua", "Grass" : "green", "Normal" : "lightsalmon", "Electric" : "yellow", "Bug" : "lightgreen", "Fighting" : "red", "Ground" : "khaki", "Rock" : "goldenrod", "Steel" : "silver", "Poison" : "purple", "Psychic" : "palevioletred", "Ghost" : "rebeccapurple", "Dark" : "darkslategray", "Ice" : "cyan", "Dragon" : "dodgerblue", "Fairy" : "violet", "Flying" : "azure"}
+
+#wykres kołowy rozkładu głównych typów
 dane_wykres = dane[["Type 1", "#"]].groupby(dane["Type 1"]).count()
-plt.pie(dane_wykres["#"], labels=dane_wykres.index, autopct="%1.1f%%")
+plt.pie(dane_wykres["#"], labels=dane_wykres.index, autopct="%1.1f%%", colors=[colors[key] for key in dane_wykres.index])
 plt.gcf().set_size_inches(8, 8)
 plt.title("Rozkład głównych typów")
 plt.savefig("./wykresy/rozklad_glownych_typow.jpg")
 plt.clf()
-
+#wykres kołowy rozkładu drugorzędnych typów
+dane_wykres = dane[["Type 2", "#"]].groupby(dane["Type 2"]).count()
+plt.pie(dane_wykres["#"], labels=dane_wykres.index, autopct="%1.1f%%", colors=[colors[key] for key in dane_wykres.index])
+plt.gcf().set_size_inches(8, 8)
+plt.title("Rozkład drugorzędnych typów")
+plt.savefig("./wykresy/rozklad_drugorzednych_typow.jpg")
+plt.clf()
+#wykres kołowy rozkładu wszystkich typów
 typy1 = dane[["Type 1", "#"]]
 typy2 = dane[["Type 2", "#"]]
 typy2 = typy2[~(typy2["Type 2"].isna())]
@@ -50,28 +63,34 @@ typy2["Type 1"] = typy2["Type 2"]
 typy2.drop(labels=["Type 2"],axis=1)
 typy = pd.concat([typy1,typy2])
 typy = typy.groupby(typy["Type 1"]).count()
-plt.pie(typy["#"], labels=typy.index, autopct="%1.1f%%")
+plt.pie(typy["#"], labels=typy.index, autopct="%1.1f%%", colors=[colors[key] for key in typy.index])
 plt.gcf().set_size_inches(8,8)
 plt.title("Rozkład typów z uwzględnieniem drugorzędnych")
 plt.savefig("./wykresy/rozklad_typow.jpg")
 plt.clf()
 
-
+#wykres słupkowy liczbie pokemonów o jednym lub dwóch typach
 podwojne_typy = dane.dropna()
 pojedyncze_typy = dane[dane["Type 2"].isna()]
 n_podwojnych = len(podwojne_typy)
 n_pojedynczych = len(pojedyncze_typy)
-
 
 plt.bar(["Pojedyncze typy", "Podwójne typy"], [n_pojedynczych, n_podwojnych])
 plt.title("Liczba Pokemon'ów o pojedynczym i podwójnym typie")
 plt.savefig("./wykresy/liczba_pkmn_o_liczbie_typow.jpg")
 plt.clf()
 
+#wykres TSNE dla statystyk bojowych przyjmując za etykiety typ główny
 tsne = TSNE(n_components=2,).fit_transform(dane[["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"]])
 tsne_df = pd.DataFrame({'tsne_1': tsne[:,0], 'tsne_2': tsne[:,1]})
-tsne_df['label'] = dane[["Type 1"]]
-print(tsne.shape)
-plt.scatter(x='tsne_1', y='tsne_2', c=np.random.rand(len(tsne_df.label.unique()),3), data=tsne_df)
-plt.show()
+tsne_df['type'] = dane[["Type 1"]]
+types = tsne_df.type.unique()
+for typ in types:
+	color = colors[typ]
+	dane_wykres = tsne_df[tsne_df["type"] == typ]
+	plt.scatter(x = dane_wykres[["tsne_1"]], y = dane_wykres[["tsne_2"]], c = color)
+plt.legend(types)
+plt.title("Wykres TSNE dla statystyk bojowych z etykietowaniem na podstawie głównego typu")
+plt.savefig("./wykresy/tsne.jpg")
+plt.clf()
 
